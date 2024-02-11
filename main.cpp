86239591f19d8a1db8af1e503b32a5f514269c52
Billy Arnold <billyarnoldd@gmail.com>
2024-02-11 13:01:05 +0000

    functions

--- main.cpp
+++ main.cpp
@@ -7,18 +7,27 @@
 
 #include <iostream>
 
+// function names start with their return type
+// void return type for functions that don't explicitly return anything
+void doPrint() {
+    std::cout << "Printing\n";
+}
+
 int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World!" << std::endl;//cout means character output
-    std::cout << "Another" << " Word\n"; // \n is better than endl because endl flushes the buffer
+    std::cout << "Enter a number to be multiplied" << std::endl;//cout means character output
+    //std::cout << "Another" << " Word\n"; // \n is better than endl because endl flushes the buffer
     //data types
     
-    int x; //define variable named x that is an int
+    int x{}; //define variable named x that is an int
     std::cin >> x; // character input from user to go into x
-    int a, b; //define 2 integers
+    //int a, b; //define 2 integers
     
-    double y; // define a double
+    std::cout << "Double that number is " << x * 2 << "\n";
+    doPrint();
     
-    int f = 5; //copy var, copied 5 to f
-    int h(5); //direct var, created h with 5 already there, more performant
+    //double y; // define a double
+    
+    //int f = 5; //copy var, copied 5 to f
+    //int h(5); //direct var, created h with 5 already there, more performant
     return 0;
 }
