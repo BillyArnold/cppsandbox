fd47dc2e50631b7dc9e1b79d541e4aae2a5244e6
Billy Arnold <billyarnoldd@gmail.com>
2024-02-20 21:09:56 +0000

    Starting on vertex and shader stuff

--- main.cpp
+++ main.cpp
@@ -2,6 +2,28 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
+
+//triangle vertices
+float vertices[] = {
+-0.5f, -0.5f, 0.0f,
+0.5f, -0.5f, 0.0f,
+0.0f, 0.5f, 0.0f
+};
+
+
+// vertex shaders are written in glsl shader language so get stored in a char ready to be compiled
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
 int main()
 {
     // Initialize GLFW
@@ -39,6 +61,23 @@ int main()
     // Set viewport
     glViewport(0, 0, 800, 600);
     
+    //START::: BELOW IS JUST FOR STORING THE VERTICES (STRUCTURE) IN THE GRAPHICS MEMORY
+    //generate vertex buffer with id for hello triangle
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    
+    //bind the created bugger to the array buffer
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    
+    //glbufferdata allows us to copy the vertices into the buffer memory
+    //options for the last option
+    //GL_STREAM_DRAW: the data is set only once and used by the GPU at most a few times.
+    //GL_STATIC_DRAW: the data is set only once and used many times.
+    //GL_DYNAMIC_DRAW: the data is changed a lot and used many times.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    // :::END
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -47,6 +86,16 @@ int main()
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
+        
+        /* Process input from keyboard */
+        processInput(window);
+        
+        /* Rendering commands should go here */
+        
+        //set color
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        //when cleared, the clear color will be left
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Poll for and process events */
         glfwPollEvents();
@@ -55,3 +104,5 @@ int main()
     glfwTerminate();
     return 0;
 }
+
+
